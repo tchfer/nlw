8ce8ec0e6d5a1c549c7fd02719f5bead567d6dde
Flavio fernando da Silva <tchfernando@gmail.com>
2020-08-06 15:41:05 -0300

    Estilização da página de cadastro de professores e começo de conexão do front end com back end

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -2,29 +2,46 @@ import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
+import Input from '../../components/Input';
+import Select from '../../components/Select';
 
 import './styles.css';
-
 
 function TeacherList() {
     return(
         <div id="page-teacher-list" className="container">
             <PageHeader title="Estes são os proffys disponíveis.">
                 <form id="search-teachers">
-                    <div className="input-block">
-                        <label htmlFor="subject">Matéria</label>
-                        <input type="text" id="subject" />
-                    </div>
-
-                    <div className="input-block">
-                        <label htmlFor="week_day">Dia da semana</label>
-                        <input type="text" id="week_day" />
-                    </div>
-
-                    <div className="input-block">
-                        <label htmlFor="time">Horário</label>
-                        <input type="text" id="time" />
-                    </div>
+                    <Select 
+                        name="subject" 
+                        label="Matéria"
+                        options={[
+                            { value:'Língua Portuguesa', label: 'Língua Portuguesa' },
+                            { value:'Matemática', label: 'Matemática' },
+                            { value:'Língua Inglesa', label: 'Língua Inglesa' },
+                            { value:'Biologia', label: 'Biologia' },
+                            { value:'Química', label: 'Química' },
+                            { value:'Física', label: 'Física' },
+                            { value:'História', label: 'História' },
+                            { value:'Geografia', label: 'Geografia' },
+                            { value:'Filosofia', label: 'Filosofia' },
+                            { value:'Sociologia', label: 'Sociologia' }, 
+                        ]}
+                    />
+                    <Select 
+                        name="week_day" 
+                        label="Dia da semana"
+                        options={[
+                            { value:'0', label: 'Domingo' },
+                            { value:'1', label: 'Segunda-feira' },
+                            { value:'2', label: 'Terça-feira' },
+                            { value:'3', label: 'Quarta-feira' },
+                            { value:'4', label: 'Quinta-feira' },
+                            { value:'5', label: 'Sexta-feira' },
+                            { value:'6', label: 'Sábado' },
+                        ]}
+                    />
+                    <Input type="time" name="time" label="Horário" />
                 </form>
             </PageHeader>
 
