2bffc388cbc110a712eef03f7dd13b57713d4085
Flavio fernando da Silva <tchfernando@gmail.com>
2020-08-08 03:17:26 -0300

    Estilização do cabeçalho, lista de professores e favoritos

--- mobile/src/pages/Favourites/index.tsx
+++ mobile/src/pages/Favourites/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import { View } from 'react-native';
 
+import PageHeader from '../../components/PageHeader';
+
 import styles from './styles';
 
+
 function Favourites() {
-    return <View style={styles.container} />
+    return (
+    <View style={styles.container}>
+        <PageHeader title="Meus proffys favoritos" />
+    </View>
+    )
 }
 
 export default Favourites;
