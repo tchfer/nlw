fe5b8af34f0f38ec09863f372834a9a579bf26d6
Flavio fernando da Silva <tchfernando@gmail.com>
2020-08-08 02:10:05 -0300

    Links da página inicial com as páginas Estudar e Dar aulas

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,13 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import Landing from './src/pages/Landing';
-import { AppLoading } from 'expo';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { AppLoading } from "expo";
+import {
+  Archivo_400Regular,
+  Archivo_700Bold,
+  useFonts,
+} from "@expo-google-fonts/archivo";
+import {
+  Poppins_400Regular,
+  Poppins_600SemiBold,
+} from "@expo-google-fonts/poppins";
 
-import { Archivo_400Regular, Archivo_700Bold, useFonts } from '@expo-google-fonts/archivo';
-import { Poppins_400Regular, Poppins_600SemiBold } from '@expo-google-fonts/poppins';
+import AppStack from "./src/routes/AppStack";
 
 export default function App() {
-  let [fontsLoaded] = useFonts ({
+  let [fontsLoaded] = useFonts({
     Archivo_400Regular,
     Archivo_700Bold,
     Poppins_400Regular,
@@ -18,9 +25,9 @@ export default function App() {
     return <AppLoading />;
   } else {
     return (
-      <> 
-      <Landing />
-      <StatusBar style="light" />
+      <>
+        <AppStack />
+        <StatusBar style="light" />
       </>
     );
   }
